e04c24a3b79ef794f4d4e803ab1fd0077bef7a3c
Caian Ertl <hi@caian.org>
2022-04-22 00:32:52 -0300

    improve error checking when building and add script to lint all

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,65 +1,63 @@
 #!/usr/bin/env bash
 
-set -e
+source ./shared.sh
 
 DOCKER_ACC_USERNAME="caian"
 HERE=$(pwd)
 
-# [e]cho [i]ndented
-ei() {
-    echo "  ${1}"
-}
-
-# [e]cho [w]aiting
-ew() {
-    printf "  %s" "$1"
-}
-
-# [e]cho [f]inished
-ef() {
-    printf " %s\n" "$1"
-}
-
-newl() {
-    echo ""
-}
 
 check_and_build() {
     ew "Checking remote repository..."
 
+    local repo
+    local tag
+    repo="$1"
+    tag="$2"
+
     local proj_tagged
     local proj_latest
-
-    proj_tagged="${DOCKER_ACC_USERNAME}/${1}:${2}"
-    proj_latest="${DOCKER_ACC_USERNAME}/${1}:latest"
+    proj_tagged="${DOCKER_ACC_USERNAME}/${repo}:${tag}"
+    proj_latest="${DOCKER_ACC_USERNAME}/${repo}:latest"
 
     if docker manifest inspect "$proj_tagged" > /dev/null 2>&1; then
         ef "tag already exists; skipping"
     else
         ef "not found"
-
         ei "Building image..."
         newl
-        docker build -t "$proj_tagged" .
-        docker build -t "$proj_latest" .
-        newl
 
-        ei "Pushing to remote repository..."
-        newl
-        docker push "$proj_tagged"
-        docker push "$proj_latest"
-        newl
-        ei "FINISHED"
+        if docker build -t "$proj_tagged" .; then
+            docker build -t "$proj_latest" .
+            newl
+
+            ei "Pushing to remote repository..."
+            newl
+
+            if docker push "$proj_tagged"; then
+                docker push "$proj_latest"
+                newl
+                ei "FINISHED"
+            else
+                log "$HERE" "$repo" "Could not push image to remote"
+            fi
+            # ...
+        else
+            log "$HERE" "$repo" "Could not build image"
+        fi
     fi
 }
 
 main() {
     printf "\n%s\n" "--- ROUTINE STARTED ---"
+
     for docker_proj in */; do
         newl
         echo "ENTERING PROJECT: ${docker_proj}"
 
-        local dp_dir  # [d]ocker [p]roject dir
+        local dp_nfs  # [d]ocker [p]roject - [n]o [f]inal [s]lash
+        dp_nfs="${docker_proj%*/}"
+
+        local dp_dir  # [d]ocker [p]roject - dir
         dp_dir="${HERE}/${docker_proj}"
         (
             cd "$dp_dir"
@@ -67,17 +65,28 @@ main() {
             local df_path  # [d]ocker[f]ile path
             df_path="${dp_dir}/Dockerfile"
 
-            if [ -f "$df_path" ]; then
+            if [[ -f "$df_path" ]]; then
                 local version
                 version=$(head -1 "$df_path" | awk '{split($0, a, "="); print a[2]}' | xargs)
-                ei "Found with version ${version}"
 
-                check_and_build "${docker_proj%*/}" "$version"
+                if [[ "$version" == "" ]]; then
+                    local err_msg
+                    err_msg="Could not retrieve version from Dockerfile; file is malformed, skipping"
+
+                    ei "$err_msg"
+                    log "$HERE" "$dp_nfs" "$err_msg"
+                else
+                    ei "Found with version ${version}"
+                    check_and_build "$dp_nfs" "$version"
+                fi
             else
                 ei "Dockerfile not found; skipping"
             fi
         )
     done
+
+    see_logs "$HERE"
+    printf "\n%s\n" "--- ROUTINE FINISHED ---"
 }
 
 main
